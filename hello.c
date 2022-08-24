8b9f28e2610869d6de2e389da9bbcbc16945f7b9
Ryan <rplambeck3@huskers.unl.edu>
2022-08-24 17:48:18 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Ryan Plambeck
+ *        rplambeck3@unl.edu or ryanplambeck@unomaha.edu
+ * Date: 2022/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ryan\n");
+  return 0;
